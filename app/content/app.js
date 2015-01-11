4b3c20d59d0053bed4d7c7641da79fae750883b4
Yoann Schwartz <schwartz@polytech.unice.fr>
2015-01-11 01:03:22 +0100

    setup part2

--- app/content/app.js
+++ app/content/app.js
@@ -7,24 +7,33 @@ var phonecatApp = angular.module('ihmAlzheimerApp', [
   'phonecatAnimations',
   'phonecatControllers',
   'phonecatFilters',
-  'phonecatServices'
+  'phonecatServices',
+  'ihm_alzheimer_services'
 ]);
 
 phonecatApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/phones', {
-        templateUrl: 'content/partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'content/partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
-      });
-  }]);
+    function($routeProvider) {
+        $routeProvider.
+            when('/phones', {
+                templateUrl: 'content/partials/phone-list.html',
+                controller: 'PhoneListCtrl'
+            }).
+            when('/phones/:phoneId', {
+                templateUrl: 'content/partials/phone-detail.html',
+                controller: 'PhoneDetailCtrl'
+            }).
+            when('/',{
+                templateUrl: 'content/menu/menu.html',
+                controller: 'MenuCtrl'
+            }).
+            when('/albums',{
+                templateUrl: 'content/album/album.html',
+                controller: 'PhoneDetailCtrl'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
 
 /*
 var phonecatApp = angular.module('phonecatApp', [
